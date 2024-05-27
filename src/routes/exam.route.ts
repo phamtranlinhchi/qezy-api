01412a446d6db4ab6c5602a7a83650be25437ea3
Linh Chi <admin@admins-MacBook-Pro.local>
2024-05-28 02:01:23 +0700

    Remove isAdmin middleware

--- src/routes/exam.route.ts
+++ src/routes/exam.route.ts
@@ -6,17 +6,17 @@ export const examRouter: Router = express.Router();
 
 // /api/exams/
 
-examRouter.get('/all', auth, isAdmin, examController.getAllExams);
-examRouter.get('/temp/:id', auth, isAdmin, examController.getExamTempById);
-examRouter.get('/:id', auth, isAdmin, examController.getExamById);
-examRouter.get('/', auth, isAdmin, examController.getExams);
+examRouter.get('/all', auth, examController.getAllExams);
+examRouter.get('/temp/:id', auth, examController.getExamTempById);
+examRouter.get('/:id', auth, examController.getExamById);
+examRouter.get('/', auth, examController.getExams);
 
-examRouter.post('/', auth, isAdmin, examController.createExam);
+examRouter.post('/', auth, examController.createExam);
 
-examRouter.put('/:id', auth, isAdmin,
+examRouter.put('/:id', auth,
   // authorizeCreator,
   examController.updateExam);
 
-examRouter.delete('/:id', auth, isAdmin,
+examRouter.delete('/:id', auth,
   // authorizeCreator,
   examController.deleteExam);
--- src/routes/question.route.ts
+++ src/routes/question.route.ts
@@ -6,20 +6,20 @@ export const questionRouter: Router = express.Router();
 
 // /api/questions/
 
-questionRouter.get('/all', auth, isAdmin, questionController.getAllQuestions);
+questionRouter.get('/all', auth, questionController.getAllQuestions);
 questionRouter.get('/:id', auth,
-  isAdmin, questionController.getQuestionById);
+  questionController.getQuestionById);
 questionRouter.get('/', auth,
-  isAdmin, questionController.getQuestions);
+  questionController.getQuestions);
 
 questionRouter.post('/', auth,
-  isAdmin, questionController.createQuestion);
+  questionController.createQuestion);
 
 questionRouter.put(
   '/:id',
   auth,
   // authorizeCreator,
-  isAdmin,
+
   questionController.updateQuestion
 );
 
@@ -27,6 +27,6 @@ questionRouter.delete(
   '/:id',
   auth,
   // authorizeCreator,
-  isAdmin,
+
   questionController.deleteQuestion
 );
